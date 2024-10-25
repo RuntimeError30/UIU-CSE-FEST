625803476b5c3df5b0e9d349e4accb28741246f2
System Administrator <root@mdkFahim30s-MacBook-Air.local>
2024-10-25 10:59:53 +0600

    Landing

---
+++ src/Components/landing/landing.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { gsap } from "gsap";
+
+  let scrollY = 0;
+  let ticker;
+
+  // Scroll and move text using GSAP
+  window.addEventListener('scroll', () => {
+    scrollY = -2 * window.scrollY; // Adjust the scroll factor for speed
+    gsap.to(ticker, { x: scrollY, ease: "power1.out", duration: 0.1 });
+  });
+</script>
+
+<section>
+    <div>
+        <h1 bind:this={ticker} style="font-weight: 900; font-size: 15rem; white-space: nowrap;">UIU CSE FEST 2025 UIU CSE FEST 2025 UIU CSE FEST 2025</h1>
+        <h1 style="display: flex; justify-content: center; font-size: 2rem;">Scroll to continue</h1>
+    </div>
+</section>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import Nav from '../Components/nav/nav.svelte';
 </script>
+
+<Nav />
 
 <slot />

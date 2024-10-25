00cba4e05b55905dee90f7cc26fd71a7047ae230
Md. Darain Khan <baxe2030@gmail.com>
2024-10-25 20:37:58 +0600

    several works have done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script>
 	import '../app.css';
 	import Nav from '../Components/nav/nav.svelte';
+
+	let scpos = 0;
+
+	function handleScroll() {
+		scpos = window.scrollY;
+		console.log('scroll: ', { scpos });
+	}
 </script>
 
-<Nav />
+<main class="">
+	<Nav />
+</main>
 
 <slot />
